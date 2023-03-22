09a9919fbb1cb3b679fb452bd1a706ccded2d2af
Oleg Vodolazsky <oleg@supersaas.com>
2023-03-22 15:21:58 +0100

    If display_choice is equal to popup_btn - display a popup button instead of a button that forwards a user to supersaas

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -29,47 +29,55 @@ function supersaas_button_hook( $atts ) {
 
 	$account = get_option( 'ss_account_name' );
 	$api_key = get_option( 'ss_password' );
+	$display_choice = get_option( 'ss_display_choice' );
+    $widget_script = get_option( 'ss_widget_script' );
 
-	if ( $account && $api_key && $after ) {
-		if ( ! $label ) {
-			$label = __( 'Book Now!', 'supersaas' );
-		}
+    if($display_choice === 'regular_btn') {
+        if ( $account && $api_key && $after ) {
+        	if ( ! $label ) {
+        		$label = __( 'Book Now!', 'supersaas' );
+        	}
 
-		$domain = get_option( 'ss_domain' );
-		$user_login = $current_user->user_login;
-		$protocol = isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] == 'on' || isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ? 'https://' : 'http://';
+        	$domain = get_option( 'ss_domain' );
+        	$user_login = $current_user->user_login;
+        	$protocol = isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] == 'on' || isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ? 'https://' : 'http://';
 
-		if ( ! $domain ) {
-			$api_endpoint = 'https://' . __( 'www.supersaas.com', 'supersaas' ) . '/api/users';
-		} elseif ( filter_var( $domain, FILTER_VALIDATE_URL ) ) {
-			$api_endpoint = rtrim( $domain, '/' ) . '/api/users';
-		} else {
-			$api_endpoint = $protocol . rtrim( $domain, '/' ) . '/api/users';
-		}
+        	if ( ! $domain ) {
+        		$api_endpoint = 'https://' . __( 'www.supersaas.com', 'supersaas' ) . '/api/users';
+        	} elseif ( filter_var( $domain, FILTER_VALIDATE_URL ) ) {
+        		$api_endpoint = rtrim( $domain, '/' ) . '/api/users';
+        	} else {
+        		$api_endpoint = $protocol . rtrim( $domain, '/' ) . '/api/users';
+        	}
 
-		$account = str_replace( ' ', '_', $account );
-		$out = '<form method="post" action=' . $api_endpoint . '>';
-		$out .= '<input type="hidden" name="account" value="' . $account . '"/>';
-		$out .= '<input type="hidden" name="id" value="' . $current_user->ID . 'fk"/>';
-		$out .= '<input type="hidden" name="user[name]" value="' . htmlspecialchars( $user_login ) . '"/>';
-		$out .= '<input type="hidden" name="user[full_name]" value="' . htmlspecialchars( $current_user->user_firstname . ' ' . $current_user->user_lastname ) . '"/>';
-		$out .= '<input type="hidden" name="user[email]" value="' . htmlspecialchars( $current_user->user_email ) . '"/>';
-		$out .= '<input type="hidden" name="checksum" value="' . md5( "$account$api_key$user_login" ) . '"/>';
-		$out .= '<input type="hidden" name="after" value="' . htmlspecialchars( str_replace( ' ', '_', $after ) ) . '"/>';
+        	$account = str_replace( ' ', '_', $account );
+        	$out = '<form method="post" action=' . $api_endpoint . '>';
+        	$out .= '<input type="hidden" name="account" value="' . $account . '"/>';
+        	$out .= '<input type="hidden" name="id" value="' . $current_user->ID . 'fk"/>';
+        	$out .= '<input type="hidden" name="user[name]" value="' . htmlspecialchars( $user_login ) . '"/>';
+        	$out .= '<input type="hidden" name="user[full_name]" value="' . htmlspecialchars( $current_user->user_firstname . ' ' . $current_user->user_lastname ) . '"/>';
+        	$out .= '<input type="hidden" name="user[email]" value="' . htmlspecialchars( $current_user->user_email ) . '"/>';
+        	$out .= '<input type="hidden" name="checksum" value="' . md5( "$account$api_key$user_login" ) . '"/>';
+        	$out .= '<input type="hidden" name="after" value="' . htmlspecialchars( str_replace( ' ', '_', $after ) ) . '"/>';
 
-		if ( $image ) {
-			$out .= '<input type="image" src="' . $image . '" alt="' . htmlspecialchars( $label ) . '" name="submit" onclick="return  confirmBooking()"/>';
-		} else {
-			$out .= '<input type="submit" value="' . htmlspecialchars( $label ) . '" onclick="return confirmBooking()"/>';
-		}
+        	if ( $image ) {
+        		$out .= '<input type="image" src="' . $image . '" alt="' . htmlspecialchars( $label ) . '" name="submit" onclick="return  confirmBooking()"/>';
+        	} else {
+        		$out .= '<input type="submit" value="' . htmlspecialchars( $label ) . '" onclick="return confirmBooking()"/>';
+        	}
 
-		$out .= '</form><script type="text/javascript">function confirmBooking() {';
-		$out .= "var reservedWords = ['administrator','supervise','supervisor','superuser','user','admin','supersaas'];";
-		$out .= "for (i = 0; i < reservedWords.length; i++) {if (reservedWords[i] === '{$user_login}') {return confirm('";
-		$out .= __( 'Your username is a supersaas reserved word. You might not be able to login. Do you want to continue?', 'supersaas' ) . "');}}}</script>";
-	} else {
-		$out = __( '(Setup incomplete)', 'supersaas' );
-	}
+        	$out .= '</form><script type="text/javascript">function confirmBooking() {';
+        	$out .= "var reservedWords = ['administrator','supervise','supervisor','superuser','user','admin','supersaas'];";
+        	$out .= "for (i = 0; i < reservedWords.length; i++) {if (reservedWords[i] === '{$user_login}') {return confirm('";
+        	$out .= __( 'Your username is a supersaas reserved word. You might not be able to login. Do you want to continue?', 'supersaas' ) . "');}}}</script>";
+        } else {
+        	$out = __( '(Setup incomplete)', 'supersaas' );
+        }
+    }
+
+    if($display_choice === 'popup_btn') {
+        $out = $widget_script;
+    }
 
 	return $out;
 }
