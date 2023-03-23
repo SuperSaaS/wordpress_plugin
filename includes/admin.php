4e1cdbfe374a67a0577c3ebb52cd57e7eb456f40
Oleg Vodolazsky <oleg@supersaas.com>
2023-03-23 14:47:35 +0100

    Update formatting

--- includes/admin.php
+++ includes/admin.php
@@ -5,29 +5,31 @@
  * @package SuperSaaS
  */
 
-defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+defined('ABSPATH') or die('No script kiddies please!');
 
 /**
  * Adds the SuperSaaS settings page.
  */
-function supersaas_add_admin_menu() {
-	add_options_page( __( 'SuperSaaS Settings', 'supersaas' ), 'SuperSaaS', 'manage_options', 'supersaas-settings', 'supersaas_options' );
+function supersaas_add_admin_menu()
+{
+  add_options_page(__('SuperSaaS Settings', 'supersaas'), 'SuperSaaS', 'manage_options', 'supersaas-settings', 'supersaas_options');
 }
 
 /**
  * Registers the SuperSaaS settings.
  */
-function supersaas_register_settings() {
-	register_setting( 'supersaas-settings', 'ss_account_name' );
-	register_setting( 'supersaas-settings', 'ss_display_choice' ); // new
-	register_setting( 'supersaas-settings', 'ss_autologin_enabled' ); // new
-	register_setting( 'supersaas-settings', 'ss_password' ); // NOTE: this is an API KEY, not a user password; the "ss_password" key is used for backwards compatibility
-	register_setting( 'supersaas-settings', 'ss_widget_script' ); // new
+function supersaas_register_settings()
+{
+  register_setting('supersaas-settings', 'ss_account_name');
+  register_setting('supersaas-settings', 'ss_display_choice'); // new
+  register_setting('supersaas-settings', 'ss_autologin_enabled'); // new
+  register_setting('supersaas-settings', 'ss_password'); // NOTE: this is an API KEY, not a user password; the "ss_password" key is used for backwards compatibility
+  register_setting('supersaas-settings', 'ss_widget_script'); // new
 
-    register_setting( 'supersaas-settings', 'ss_schedule' );
-	register_setting( 'supersaas-settings', 'ss_button_label' );
-	register_setting( 'supersaas-settings', 'ss_button_image' );
-	register_setting( 'supersaas-settings', 'ss_domain', 'domain_from_url' );
+  register_setting('supersaas-settings', 'ss_schedule');
+  register_setting('supersaas-settings', 'ss_button_label');
+  register_setting('supersaas-settings', 'ss_button_image');
+  register_setting('supersaas-settings', 'ss_domain', 'domain_from_url');
 }
 
 /**
@@ -37,136 +39,179 @@ function supersaas_register_settings() {
  *
  * @return string The domain (and port) name part of the URL.
  */
-function domain_from_url($ss_domain) {
-	$url_parts = parse_url( $ss_domain );
-	if ( isset( $url_parts['host'] ) ) {
-		$domain = $url_parts['host'];
-		if ( isset( $url_parts['port'] ) ) {
-			$domain .= ':' . $url_parts['port'];
-		}
+function domain_from_url($ss_domain)
+{
+  $url_parts = parse_url($ss_domain);
+  if (isset($url_parts['host'])) {
+    $domain = $url_parts['host'];
+    if (isset($url_parts['port'])) {
+      $domain .= ':' . $url_parts['port'];
+    }
 
-		return $domain;
-	} else {
-		return $ss_domain;
-	}
+    return $domain;
+  } else {
+    return $ss_domain;
+  }
 }
 
 /**
  * Outputs the content of the SuperSaaS options page.
  */
-function supersaas_options() {
-	if ( ! current_user_can( 'manage_options' ) ) {
-		wp_die( __( 'You do not have sufficient permissions to access this page.' ) ); // WPCS: XSS.EscapeOutput OK.
-	}
+function supersaas_options()
+{
+  if (!current_user_can('manage_options')) {
+    wp_die(__('You do not have sufficient permissions to access this page.')); // WPCS: XSS.EscapeOutput OK.
+  }
 
 
-?>
-<div class="wrap">
-	<h2><?php _e( 'SuperSaaS Settings', 'supersaas' ); // WPCS: XSS.EscapeOutput OK. ?></h2>
+  ?>
+  <div class="wrap">
+    <h2><?php _e('SuperSaaS Settings', 'supersaas'); // WPCS: XSS.EscapeOutput OK.
+      ?></h2>
 
-	<form method="post" action="options.php">
-	<?php settings_fields( 'supersaas-settings' ); ?>
-
-
-    <p>
-        <?php _e( 'SuperSaaS account name', 'supersaas' ); // WPCS: XSS.EscapeOutput OK. ?>
+    <form method="post" action="options.php">
+      <?php settings_fields('supersaas-settings'); ?>
+      <p>
+        <?php _e('SuperSaaS account name', 'supersaas'); // WPCS: XSS.EscapeOutput OK.
+        ?>
         <em>:</em>
-        <input type="text" name="ss_account_name" value="<?php echo get_option( 'ss_account_name' ); // WPCS: XSS.EscapeOutput OK. ?>" required /><br />
-    </p>
+        <input type="text" name="ss_account_name"
+               value="<?php echo get_option('ss_account_name'); // WPCS: XSS.EscapeOutput OK.
+               ?>"
+               required/><br/>
+      </p>
 
-	<p>
-	    How would you like to show your SuperSaaS schedule?
-	</p>
+      <p>
+        How would you like to show your SuperSaaS schedule?
+      </p>
 
-	<fieldset>
+      <fieldset>
         <legend class="screen-reader-text"><span>Your homepage displays</span></legend>
         <p>
-            <label>
-                <input name="ss_display_choice" type="radio" value="regular_btn" class="tog" <?php echo get_option( 'ss_display_choice' ) === 'regular_btn' ? 'checked' : ''; // WPCS: XSS.EscapeOutput OK. ?> />
-                Show a button that forwards the user to my SuperSaaS calendar
-            </label>
+          <label>
+            <input name="ss_display_choice" type="radio" value="regular_btn"
+                   class="tog" <?php echo get_option('ss_display_choice') === 'regular_btn' ? 'checked' : ''; // WPCS: XSS.EscapeOutput OK.
+            ?> />
+            Show a button that forwards the user to my SuperSaaS calendar
+          </label>
         </p>
         <p>
-            <label>
-                <input name="ss_display_choice" type="radio" value="frame" class="tog" <?php echo get_option( 'ss_display_choice' ) === 'frame' ? 'checked' : ''; // WPCS: XSS.EscapeOutput OK. ?> />
-                Show the calendar inside a small frame on my Wordpress site
-            </label>
+          <label>
+            <input name="ss_display_choice" type="radio" value="frame"
+                   class="tog" <?php echo get_option('ss_display_choice') === 'frame' ? 'checked' : ''; // WPCS: XSS.EscapeOutput OK.
+            ?> />
+            Show the calendar inside a small frame on my Wordpress site
+          </label>
         </p>
         <p>
-            <label>
-                <input name="ss_display_choice" type="radio" value="popup_btn" class="tog" <?php echo get_option( 'ss_display_choice' ) === 'popup_btn' ? 'checked' : ''; // WPCS: XSS.EscapeOutput OK. ?> />
-                Show a button that displays a popup on my site containing the calendar
-            </label>
+          <label>
+            <input name="ss_display_choice" type="radio" value="popup_btn"
+                   class="tog" <?php echo get_option('ss_display_choice') === 'popup_btn' ? 'checked' : ''; // WPCS: XSS.EscapeOutput OK.
+            ?> />
+            Show a button that displays a popup on my site containing the calendar
+          </label>
         </p>
-    </fieldset>
+      </fieldset>
 
-    <p>
+      <p>
         <label>
-            <input type="checkbox" name="ss_autologin_enabled" value="1" <?php echo get_option( 'ss_autologin_enabled' ) === '1' ? 'checked' : ''; // WPCS: XSS.EscapeOutput OK. ?> />
-            If the user is logged into Wordpress, log him into your SuperSaaS account with the Wordpress user name
+          <input type="checkbox" name="ss_autologin_enabled"
+                 value="1" <?php echo get_option('ss_autologin_enabled') === '1' ? 'checked' : ''; // WPCS: XSS.EscapeOutput OK.
+          ?> />
+          If the user is logged into Wordpress, log him into your SuperSaaS account with the Wordpress user
+          name
         </label>
 
         <br/>
-        Automatically logging in the user requires your API key: <input type="text" name="ss_password" value="<?php echo get_option( 'ss_password' ); // WPCS: XSS.EscapeOutput OK. ?>" />
-    </p>
+        Automatically logging in the user requires your API key: <input type="text" name="ss_password"
+                                                                        value="<?php echo get_option('ss_password'); // WPCS: XSS.EscapeOutput OK.
+                                                                        ?>"/>
+      </p>
 
-    <p>
+      <p>
         Paste your button script here:
         <br/>
-        <textarea name="ss_widget_script" rows="4" cols="80" placeholder="Paste the script generated by https://supersaas.com/info/doc/integration/integration_with_widget" >
-            <?php echo get_option( 'ss_widget_script' ); // WPCS: XSS.EscapeOutput OK. ?>
-        </textarea>
-    </p>
+        <textarea name="ss_widget_script" rows="4" cols="80"
+                  placeholder="Paste the script generated by https://supersaas.com/info/doc/integration/integration_with_widget">
+                  <?php echo get_option('ss_widget_script'); // WPCS: XSS.EscapeOutput OK.
+                  ?>
+              </textarea>
+      </p>
 
-	<table class="form-table">
+      <table class="form-table">
         <tr valign="top">
-            <th scope="row">
-                <?php _e( 'Schedule name', 'supersaas' ); // WPCS: XSS.EscapeOutput OK. ?>
-            </th>
-            <td>
-                <input type="text" name="ss_schedule" value="<?php echo get_option( 'ss_schedule' ); // WPCS: XSS.EscapeOutput OK. ?>" /><br />
-                <span class='description'><?php _e( 'The name of the schedule or URL to redirect to after login.', 'supersaas' ); // WPCS: XSS.EscapeOutput OK. ?></span>
-            </td>
+          <th scope="row">
+            <?php _e('Schedule name', 'supersaas'); // WPCS: XSS.EscapeOutput OK.
+            ?>
+          </th>
+          <td>
+            <input type="text" name="ss_schedule"
+                   value="<?php echo get_option('ss_schedule'); // WPCS: XSS.EscapeOutput OK.
+                   ?>"/><br/>
+            <span
+                class='description'><?php _e('The name of the schedule or URL to redirect to after login.', 'supersaas'); // WPCS: XSS.EscapeOutput OK.
+              ?></span>
+          </td>
         </tr>
-		<tr valign="top">
-			<th scope="row">
-				<?php _e( 'Button Label', 'supersaas' ); // WPCS: XSS.EscapeOutput OK. ?>
-				<em>(<?php _e( 'optional', 'supersaas' ); // WPCS: XSS.EscapeOutput OK. ?>)</em>
-			</th>
-			<td>
-				<input type="text" name="ss_button_label" value="<?php echo get_option( 'ss_button_label' ) ? get_option( 'ss_button_label' ) : __( 'Book Now!', 'supersaas' ); // WPCS: XSS.EscapeOutput OK. ?>" /><br />
-	  			<span class='description'><?php _e( "The text to be put on the button that is displayed, for example 'Create Appointment'.", 'supersaas' ); // WPCS: XSS.EscapeOutput OK. ?></span>
-			</td>
-	  	</tr>
+        <tr valign="top">
+          <th scope="row">
+            <?php _e('Button Label', 'supersaas'); // WPCS: XSS.EscapeOutput OK.
+            ?>
+            <em>(<?php _e('optional', 'supersaas'); // WPCS: XSS.EscapeOutput OK.
+              ?>)</em>
+          </th>
+          <td>
+            <input type="text" name="ss_button_label"
+                   value="<?php echo get_option('ss_button_label') ? get_option('ss_button_label') : __('Book Now!', 'supersaas'); // WPCS: XSS.EscapeOutput OK.
+                   ?>"/><br/>
+            <span
+                class='description'><?php _e("The text to be put on the button that is displayed, for example 'Create Appointment'.", 'supersaas'); // WPCS: XSS.EscapeOutput OK.
+              ?></span>
+          </td>
+        </tr>
 
-		<tr valign="top">
-			<th scope="row">
-				<?php _e( 'Button Image', 'supersaas' ); // WPCS: XSS.EscapeOutput OK. ?>
-				<em>(<?php _e( 'optional', 'supersaas' ); // WPCS: XSS.EscapeOutput OK. ?>)</em>
-			</th>
-			<td>
-				<input type="text" name="ss_button_image" value="<?php echo get_option( 'ss_button_image' ); // WPCS: XSS.EscapeOutput OK. ?>" /><br />
-				<span class='description'><?php _e( 'Location of an image file to use as the button. Can be left blank.', 'supersaas' ); // WPCS: XSS.EscapeOutput OK. ?></span>
-			</td>
-	 	 </tr>
+        <tr valign="top">
+          <th scope="row">
+            <?php _e('Button Image', 'supersaas'); // WPCS: XSS.EscapeOutput OK.
+            ?>
+            <em>(<?php _e('optional', 'supersaas'); // WPCS: XSS.EscapeOutput OK.
+              ?>)</em>
+          </th>
+          <td>
+            <input type="text" name="ss_button_image"
+                   value="<?php echo get_option('ss_button_image'); // WPCS: XSS.EscapeOutput OK.
+                   ?>"/><br/>
+            <span
+                class='description'><?php _e('Location of an image file to use as the button. Can be left blank.', 'supersaas'); // WPCS: XSS.EscapeOutput OK.
+              ?></span>
+          </td>
+        </tr>
 
-		<tr valign="top">
-			<th scope="row">
-				<?php _e( 'Custom domain name', 'supersaas' ); // WPCS: XSS.EscapeOutput OK. ?>
-				<em>(<?php _e( 'optional', 'supersaas' ); // WPCS: XSS.EscapeOutput OK. ?>)</em>
-			</th>
-			<td>
-				<input type="text" name="ss_domain" value="<?php echo get_option( 'ss_domain' ); // WPCS: XSS.EscapeOutput OK. ?>" /><br />
-				<span class='description'><?php _e( 'If you created a custom domain name that points to SuperSaaS enter it here. Can be left blank.', 'supersaas' ); // WPCS: XSS.EscapeOutput OK. ?></span>
-			</td>
-  		</tr>
-	</table>
+        <tr valign="top">
+          <th scope="row">
+            <?php _e('Custom domain name', 'supersaas'); // WPCS: XSS.EscapeOutput OK.
+            ?>
+            <em>(<?php _e('optional', 'supersaas'); // WPCS: XSS.EscapeOutput OK.
+              ?>)</em>
+          </th>
+          <td>
+            <input type="text" name="ss_domain"
+                   value="<?php echo get_option('ss_domain'); // WPCS: XSS.EscapeOutput OK.
+                   ?>"/><br/>
+            <span
+                class='description'><?php _e('If you created a custom domain name that points to SuperSaaS enter it here. Can be left blank.', 'supersaas'); // WPCS: XSS.EscapeOutput OK.
+              ?></span>
+          </td>
+        </tr>
+      </table>
 
-	<p class="submit">
-		<input type="submit" class="button-primary" value="<?php _e( 'Save Changes' ); // WPCS: XSS.EscapeOutput OK. ?>" />
-	</p>
-  </form>
-</div>
+      <p class="submit">
+        <input type="submit" class="button-primary"
+               value="<?php _e('Save Changes'); // WPCS: XSS.EscapeOutput OK.
+               ?>"/>
+      </p>
+    </form>
+  </div>
 
-<?php
+  <?php
 }
--- includes/shortcode.php
+++ includes/shortcode.php
@@ -5,112 +5,113 @@
  * @package SuperSaaS
  */
 
-defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+defined('ABSPATH') or die('No script kiddies please!');
 
 /**
  * Displays the SuperSaaS button.
  *
  * @param array $atts SuperSaaS shortcode attributes.
  */
-function supersaas_button_hook( $atts ) {
-	global $current_user;
-	wp_get_current_user();
-	if ( ! $current_user->ID ) {
-		return '';
-	}
+function supersaas_button_hook($atts)
+{
+  global $current_user;
+  wp_get_current_user();
+  if (!$current_user->ID) {
+    return '';
+  }
 
-	extract(shortcode_atts(
-		array(
-			'label' => get_option( 'ss_button_label', '' ),
-			'image' => get_option( 'ss_button_image', '' ),
-			'after' => '',
-			'schedule' => '',
-		), $atts, 'supersaas'
-	));
+  extract(shortcode_atts(
+    array(
+      'label' => get_option('ss_button_label', ''),
+      'image' => get_option('ss_button_image', ''),
+      'after' => '',
+      'schedule' => '',
+    ), $atts, 'supersaas'
+  ));
 
-	$account = get_option( 'ss_account_name' );
-	$api_key = get_option( 'ss_password' );
-	$display_choice = get_option( 'ss_display_choice' );
-    $widget_script = get_option( 'ss_widget_script' );
-    $default_schedule = get_option( 'ss_schedule');
+  $account = get_option('ss_account_name');
+  $api_key = get_option('ss_password');
+  $display_choice = get_option('ss_display_choice');
+  $widget_script = get_option('ss_widget_script');
+  $default_schedule = get_option('ss_schedule');
 
-    $pattern = "/(?<=\")[0-9]+:\w+(?=\")/i";
-    preg_match_all($pattern, $widget_script, $matches);
+  $pattern = "/(?<=\")[0-9]+:\w+(?=\")/i";
+  preg_match_all($pattern, $widget_script, $matches);
 
-    if($display_choice === 'popup_btn') {
-        $out = '<div>';
-        foreach ($matches as &$match_value) {
-            foreach ($match_value as &$submatch_value) {
-                $out .= '<p>';
-                list($id, $name) = explode(':', $submatch_value);
-                if($name !== $account) {
-                    if(!empty($after)) {
-                        $out .= '<p> ' . '$after: ' . $after . '</p>';
-                        $widget_script = str_replace($submatch_value, $after, $widget_script);
-                        $widget_script = str_replace($id, $after, $widget_script);
-                    }
-                    if(!empty($schedule)) {
-                        $out .= '<p> ' . '$schedule: ' . $schedule . '</p>';
-                        $widget_script = str_replace($submatch_value, $schedule, $widget_script);
-                        $widget_script = str_replace($id, $after, $widget_script);
-                    }
-                }
-                $out .= 'extracted id: ' . $id . ' extracted name: ' . $name;
-                $out .= '</p>';
-            }
+  if ($display_choice === 'popup_btn') {
+    $out = '<div>';
+    foreach ($matches as &$match_value) {
+      foreach ($match_value as &$submatch_value) {
+        $out .= '<p>';
+        list($id, $name) = explode(':', $submatch_value);
+        if ($name !== $account) {
+          if (!empty($after)) {
+            $out .= '<p> ' . '$after: ' . $after . '</p>';
+            $widget_script = str_replace($submatch_value, $after, $widget_script);
+            $widget_script = str_replace($id, $after, $widget_script);
+          }
+          if (!empty($schedule)) {
+            $out .= '<p> ' . '$schedule: ' . $schedule . '</p>';
+            $widget_script = str_replace($submatch_value, $schedule, $widget_script);
+            $widget_script = str_replace($id, $after, $widget_script);
+          }
         }
-        $out .= '</div>';
-        $out .= $widget_script;
+        $out .= 'extracted id: ' . $id . ' extracted name: ' . $name;
+        $out .= '</p>';
+      }
     }
+    $out .= '</div>';
+    $out .= $widget_script;
+  }
 
-    if(empty($after)) {
-        // Can't replace $after in case of $display_choice === 'popup_btn' since
-        //  whether its provided is a part of the display logic for the popup
-        $after = $default_schedule;
+  if (empty($after)) {
+    // Can't replace $after in case of $display_choice === 'popup_btn' since
+    //  whether its provided is a part of the display logic for the popup
+    $after = $default_schedule;
+  }
+
+  if ($display_choice === 'regular_btn') {
+    if ($account && $api_key && $after) {
+      if (!$label) {
+        $label = __('Book Now!', 'supersaas');
+      }
+
+      $domain = get_option('ss_domain');
+      $user_login = $current_user->user_login;
+      $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ? 'https://' : 'http://';
+
+      if (!$domain) {
+        $api_endpoint = 'https://' . __('www.supersaas.com', 'supersaas') . '/api/users';
+      } elseif (filter_var($domain, FILTER_VALIDATE_URL)) {
+        $api_endpoint = rtrim($domain, '/') . '/api/users';
+      } else {
+        $api_endpoint = $protocol . rtrim($domain, '/') . '/api/users';
+      }
+
+      $account = str_replace(' ', '_', $account);
+      $out = '<form method="post" action=' . $api_endpoint . '>';
+      $out .= '<input type="hidden" name="account" value="' . $account . '"/>';
+      $out .= '<input type="hidden" name="id" value="' . $current_user->ID . 'fk"/>';
+      $out .= '<input type="hidden" name="user[name]" value="' . htmlspecialchars($user_login) . '"/>';
+      $out .= '<input type="hidden" name="user[full_name]" value="' . htmlspecialchars($current_user->user_firstname . ' ' . $current_user->user_lastname) . '"/>';
+      $out .= '<input type="hidden" name="user[email]" value="' . htmlspecialchars($current_user->user_email) . '"/>';
+      $out .= '<input type="hidden" name="checksum" value="' . md5("$account$api_key$user_login") . '"/>';
+      $out .= '<input type="hidden" name="after" value="' . htmlspecialchars(str_replace(' ', '_', $after)) . '"/>';
+
+      if ($image) {
+        $out .= '<input type="image" src="' . $image . '" alt="' . htmlspecialchars($label) . '" name="submit" onclick="return  confirmBooking()"/>';
+      } else {
+        $out .= '<input type="submit" value="' . htmlspecialchars($label) . '" onclick="return confirmBooking()"/>';
+      }
+
+      $out .= '</form><script type="text/javascript">function confirmBooking() {';
+      $out .= "var reservedWords = ['administrator','supervise','supervisor','superuser','user','admin','supersaas'];";
+      $out .= "for (i = 0; i < reservedWords.length; i++) {if (reservedWords[i] === '{$user_login}') {return confirm('";
+      $out .= __('Your username is a supersaas reserved word. You might not be able to login. Do you want to continue?', 'supersaas') . "');}}}</script>";
+    } else {
+      $out = __('(Setup incomplete)', 'supersaas');
     }
+  }
 
-    if($display_choice === 'regular_btn') {
-        if ( $account && $api_key && $after ) {
-        	if ( ! $label ) {
-        		$label = __( 'Book Now!', 'supersaas' );
-        	}
-
-        	$domain = get_option( 'ss_domain' );
-        	$user_login = $current_user->user_login;
-        	$protocol = isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] == 'on' || isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' ? 'https://' : 'http://';
-
-        	if ( ! $domain ) {
-        		$api_endpoint = 'https://' . __( 'www.supersaas.com', 'supersaas' ) . '/api/users';
-        	} elseif ( filter_var( $domain, FILTER_VALIDATE_URL ) ) {
-        		$api_endpoint = rtrim( $domain, '/' ) . '/api/users';
-        	} else {
-        		$api_endpoint = $protocol . rtrim( $domain, '/' ) . '/api/users';
-        	}
-
-        	$account = str_replace( ' ', '_', $account );
-        	$out = '<form method="post" action=' . $api_endpoint . '>';
-        	$out .= '<input type="hidden" name="account" value="' . $account . '"/>';
-        	$out .= '<input type="hidden" name="id" value="' . $current_user->ID . 'fk"/>';
-        	$out .= '<input type="hidden" name="user[name]" value="' . htmlspecialchars( $user_login ) . '"/>';
-        	$out .= '<input type="hidden" name="user[full_name]" value="' . htmlspecialchars( $current_user->user_firstname . ' ' . $current_user->user_lastname ) . '"/>';
-        	$out .= '<input type="hidden" name="user[email]" value="' . htmlspecialchars( $current_user->user_email ) . '"/>';
-        	$out .= '<input type="hidden" name="checksum" value="' . md5( "$account$api_key$user_login" ) . '"/>';
-        	$out .= '<input type="hidden" name="after" value="' . htmlspecialchars( str_replace( ' ', '_', $after ) ) . '"/>';
-
-        	if ( $image ) {
-        		$out .= '<input type="image" src="' . $image . '" alt="' . htmlspecialchars( $label ) . '" name="submit" onclick="return  confirmBooking()"/>';
-        	} else {
-        		$out .= '<input type="submit" value="' . htmlspecialchars( $label ) . '" onclick="return confirmBooking()"/>';
-        	}
-
-        	$out .= '</form><script type="text/javascript">function confirmBooking() {';
-        	$out .= "var reservedWords = ['administrator','supervise','supervisor','superuser','user','admin','supersaas'];";
-        	$out .= "for (i = 0; i < reservedWords.length; i++) {if (reservedWords[i] === '{$user_login}') {return confirm('";
-        	$out .= __( 'Your username is a supersaas reserved word. You might not be able to login. Do you want to continue?', 'supersaas' ) . "');}}}</script>";
-        } else {
-        	$out = __( '(Setup incomplete)', 'supersaas' );
-        }
-    }
-
-	return $out;
+  return $out;
 }
